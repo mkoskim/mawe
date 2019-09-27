d8f002ee84e5b1dba48dc615239a38569d7d7f2d
Markus Koskimies <mkoskim@gmail.com>
2019-09-27 19:55:56 +0300

    GUI elements moved to own directory

---
+++ gui/Tkinter.py
@@ -0,0 +1,12 @@
+###############################################################################
+#
+# Tkinter GUI imports + helpers
+#
+###############################################################################
+
+from tkinter import *
+import tkinter.ttk
+from tkinter.scrolledtext import ScrolledText
+
+#print(TkVersion, TclVersion)
+
