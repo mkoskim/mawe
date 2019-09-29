e2bae9386fa653bdd1710ca9fa1e1d1d13a8fe67
Markus Koskimies <mkoskim@gmail.com>
2019-09-29 23:10:15 +0300

    Sketching

--- gui/qt/main.py
+++ gui/qt/main.py
@@ -9,6 +9,9 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 import sys
+import tools
+
+from project.Document import ET
 
 ###############################################################################
 #
@@ -25,13 +28,41 @@ class SceneGroupEdit(QTextEdit):
         self.setAutoFormatting(QTextEdit.AutoAll)
         #self.editor.selectionChanged.connect(self.update_format)
 
+        self.setMinimumWidth(500)
         self.setFont(QFont('Times', 12))
         self.setFontPointSize(12)
 
         QShortcut(QKeySequence("Ctrl+R"), self).activated.connect(self.report)
+        QShortcut(QKeySequence("Alt+L"), self).activated.connect(self.lorem)
+
+        #self.setPlainText(tools.readfile("../../Dropbox/tarinat/trillerit/ylimielet/ylimielet.moe"))
         
     def report(self): 
-        print(self.toHtml())
+        #print(self.toHtml())
+        self.Html2Mawe()
+
+    def lorem(self):
+        self.insertHtml(
+            "<div class='x'>xxx</div>" +
+            "<h1>Lorem</h1><p><b>ipsum</b> dolor sit amet, consectetur adipiscing elit, " +
+            "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+            "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+            "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
+            "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+            "culpa qui officia deserunt mollit anim id est laborum." +
+            "</p>"
+        )
+
+    def Mawe2Hml(self, tree):
+        pass
+        
+    def Html2Mawe(self, text = None):
+        if not text: text = self.toHtml()
+
+        html = ET.fromstring(text)
+        
+        ET.dump(html)
 
 ###############################################################################
 #
