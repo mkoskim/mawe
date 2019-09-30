cc947bed24c74385933e2fc47205244508c34653
Markus Koskimies <mkoskim@gmail.com>
2019-09-30 03:07:11 +0300

    Qt rejected, back to Tkinter.
    For various reasons QTextEdit does not fit well to my purposes. Tkinter looks very heavy for this purpose, because its textbox needs fixes in many places to work right. Maybe I go back to GTK which I used for previous version (moe).

--- gui/__init__.py
+++ gui/__init__.py
@@ -4,6 +4,14 @@
 #
 ###############################################################################
 
-#from gui.tkinter.main import run
-from gui.qt.main import run
+from gui.tkinter.main import run
+
+#------------------------------------------------------------------------------
+# QT: QTextEdit looked promising, but it is crap for my purposes. HTML export
+# from widget is unusable. It is hard to add schemantical elements to blocks
+# so that you know what they are - and it is even harder for spans inside
+# blocks. Hiding elements does not work.
+#------------------------------------------------------------------------------
+
+#from gui.qt.main import run
 
--- gui/qt/main.py
+++ gui/qt/main.py
@@ -19,21 +19,60 @@ from project.Document import ET
 #
 ###############################################################################
 
-class SceneGroupEdit(QTextEdit):
+def createBlockFormat(name, **kw):
+    fmt = QTextBlockFormat()
+    print(kw)
+    for key, value in kw.items():
+        if key == "background":
+            fmt.setBackground(value)
+    return fmt
+
+blockformats = [
+    createBlockFormat("plain"),
+    createBlockFormat("comment", background = QColor(200, 250, 200)),
+]
+
+class SceneGroupEdit(QPlainTextEdit):
+
+    def toggle_block(self, blocktype):
+        cursor = self.textCursor()
+        block  = cursor.block()
+        if block.userState() == blocktype: blocktype = 0
+        
+        cursor.setBlockFormat(blockformats[blocktype])
+        block.setUserState(blocktype)
+
+    def fold_comments(self):
+        self.comments = not self.comments
+        print("folding", self.comments)
+        cursor = self.textCursor()
+        
+        for index in range(self.document().blockCount()):
+            block = self.document().findBlockByNumber(index)
+            
+            print(index, block.userState())
+
+            if block.userState() == 1:
+                block.setVisible(self.comments)
+        self.repaint()
 
     def __init__(self):
         super(SceneGroupEdit, self).__init__()
 
         # Setup the QTextEdit editor configuration
-        self.setAutoFormatting(QTextEdit.AutoAll)
+        #self.setAutoFormatting(QTextEdit.AutoAll)
         #self.editor.selectionChanged.connect(self.update_format)
 
         self.setMinimumWidth(500)
         self.setFont(QFont('Times', 12))
-        self.setFontPointSize(12)
+        #self.setFontPointSize(12)
+
+        self.comments = True
 
         QShortcut(QKeySequence("Ctrl+R"), self).activated.connect(self.report)
         QShortcut(QKeySequence("Alt+L"), self).activated.connect(self.lorem)
+        QShortcut(QKeySequence("Alt+C"), self).activated.connect(lambda: self.toggle_block(1))
+        QShortcut(QKeySequence("Alt+H"), self).activated.connect(self.fold_comments)
 
         #self.setPlainText(tools.readfile("../../Dropbox/tarinat/trillerit/ylimielet/ylimielet.moe"))
         
@@ -42,16 +81,15 @@ class SceneGroupEdit(QTextEdit):
         self.Html2Mawe()
 
     def lorem(self):
-        self.insertHtml(
-            "<div class='x'>xxx</div>" +
-            "<h1>Lorem</h1><p><b>ipsum</b> dolor sit amet, consectetur adipiscing elit, " +
+        self.insertPlainText(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
             "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
             "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
             "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
             "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla " +
             "pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
             "culpa qui officia deserunt mollit anim id est laborum." +
-            "</p>"
+            "\n"
         )
 
     def Mawe2Hml(self, tree):
