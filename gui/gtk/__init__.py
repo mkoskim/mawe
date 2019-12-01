83c3719dac0c72c1d38fb478cbd7c4c2d85ca246
Markus Koskimies <mkoskim@gmail.com>
2019-12-01 15:37:00 +0200

    Sketching

--- gui/gtk/__init__.py
+++ gui/gtk/__init__.py
@@ -3,6 +3,6 @@ gi.require_version("Gtk", "3.0")
 gi.require_version("WebKit", "3.0")
 from gi.repository import Gtk, Gdk, Pango, WebKit
 
-#from gui.gtk.SceneGroupEdit import SceneGroupEdit
-from gui.gtk.WebKitEdit import SceneGroupEdit
+from gui.gtk.TextViewEdit import SceneGroupEdit
+#from gui.gtk.WebKitEdit import SceneGroupEdit
 
